ae16c8fc30ab8601e1d9ecbdb4026ac573a1437c
Yalle <yallevieira99@gmail.com>
2022-10-24 13:27:11 -0300

    Primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const nameField = document.querySelector("input");
+
+nameField.addEventListener("input", () => {
+  nameField.setCustomValidity("");
+  nameField.checkValidity();
+  console.log(nameField.checkValidity());
+});
+
+nameField.addEventListener("invalid", () => {
+  nameField.setCustomValidity("Please fill in your First Name.");
+});
